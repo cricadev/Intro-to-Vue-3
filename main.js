c21a8b43fd389a6582d8dec79392cae327f50738
CristianN998 <cristiancasallas998@gmail.com>
2022-06-11 09:29:01 -0500

    learned about how to isntance a vue app from a javascript file then mounted it into a dom element which was referenced by an id called app i learned to put javascript expresisions within double curly brackets and did the exercise

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 const product = 'Socks'
+const app = Vue.createApp({
+  data(){
+    return {
+      product: ' Socks',
+      description: 'this socks are fantastic to wear whether you are horny or not'
+    }
+  }
+});
