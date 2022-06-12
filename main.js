9fa1d35f8c8d9c12b305fab6e59b143f7cde4067
CristianN998 <cristiancasallas998@gmail.com>
2022-06-11 20:45:17 -0500

    trying to remmove the amount of classes in tailwind css to better code legibility

--- main.js
+++ main.js
@@ -3,7 +3,11 @@ const app = Vue.createApp({
   data(){
     return {
       product: ' Socks',
-      description: 'this socks are fantastic to wear whether you are horny or not'
+      description: 'this socks are fantastic to wear whether you are horny or not',
+      styles: {
+        color: 'text-green-500 text-4xl',
+        fontSize: 'text-4xl'
+      }
     }
   }
 });
